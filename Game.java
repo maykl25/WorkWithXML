473697241ee5cb516f97e87ca971ea4a4a658f1b
Сергей Шураев <70658716+maykl25@users.noreply.github.com>
2022-03-16 16:51:00 +0200

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,270 @@
+package theGame;
+import java.io.*;
+import java.util.Scanner;
+
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.dom.DOMSource;
+import java.io.File;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Node;
+
+
+
+public class Game {
+    static int VERTICAL = 3;
+    static int HORIZONTAL = 3;
+    public static boolean repeat = true;
+    public static int firstCount = 0;
+    public static int secondCount = 0;
+    public static int step = 1;
+    public static String firstPlayerName;
+    public static String secondPlayerName;
+    public static String location;
+
+    static char [][] gameField = new char [VERTICAL][HORIZONTAL];
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    static File file = new File("d:/rating.txt");
+
+
+   public static char firstPlayerSign = 'X';
+   public static char secondPlayerSign = 'O';
+   public static char initialSign = '.';
+
+    // метод по записи в файл рейтинга игроков
+   /* public static void fileWork(String name1, String name2, int rating1, int rating2information) throws IOException {
+        //File file = new File("d:/rating.txt");
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+        bufWriter.write(information);
+        bufWriter.newLine();
+        /*String rat1 = name1 + " " + rating1;
+        String rat2 = name2 + " " + rating2;
+
+        if (rating1 > rating2) {
+            bufWriter.write(rat1);
+            bufWriter.newLine();
+            bufWriter.write(rat2);
+        } else {
+            bufWriter.write(rat2);
+            bufWriter.newLine();
+            bufWriter.write(rat1);
+        }
+        bufWriter.close();
+    }*/
+
+    //начальная настройка игрового поля
+    public static void initialPlayField() {
+        for (int i = 0; i < VERTICAL; i++) {
+            for (int j = 0; j < HORIZONTAL; j++) {
+                gameField[i][j] = initialSign;
+            }
+        }
+    }
+
+    //Графическое отображение игрового поля
+    public static void printField() {
+        System.out.println("  1 2 3");
+        int count = 1;
+        for (int i = 0; i < VERTICAL; i++) {
+            for (int j = 0; j < HORIZONTAL; j++) {
+                if (j == 0) {
+                    System.out.print(count + "|");
+                    count++;
+                }
+                System.out.print(gameField[i][j] + "|");
+            }
+            System.out.println();
+        }
+    }
+
+    // запись символа на игровое поле
+    public static void setSym(int y, int x, char sym) {
+        gameField[y][x] = sym;
+    }
+
+    public static void firstPlayerStep() {
+        int x;
+        int y;
+
+        do {
+            System.out.println("Введите координаты: X Y (1-3)");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        } while (!isCellValid(y,x));
+        location = x + " " + y;
+        setSym(y,x, firstPlayerSign);
+
+    }
+
+    public static void secondPlayerStep() {
+        int x;
+        int y;
+
+        do {
+            System.out.println("Введите координаты: X Y (1-3)");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        } while (!isCellValid(y,x));
+        location = (x + 1) + " " + (y + 1);
+        setSym(y,x, secondPlayerSign);
+    }
+
+    // Проверка правильности координат
+    public static boolean isCellValid( int y, int x) {
+        if (x < 0 || y < 0 || x > HORIZONTAL - 1 || y > VERTICAL - 1) {
+            return false;
+        }
+        return gameField[y][x] == initialSign;
+    }
+
+    // проверка, есть ли свободные места на игровом поле
+    public static boolean isFieldFull() {
+        for (int i = 0; i < VERTICAL; i++) {
+            for (int j = 0; j < HORIZONTAL; j++) {
+                if (gameField[i][j] == initialSign) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkWin(char sym) {
+        // Проверка горизонтальных позиций на наличие трех подряд ячеек
+        if (gameField[0][0] == sym && gameField[0][1] == sym && gameField[0][2] == sym) {
+            return true;
+        }
+        if (gameField[1][0] == sym && gameField[1][1] == sym && gameField[1][2] == sym) {
+            return true;
+        }
+        if (gameField[2][0] == sym && gameField[2][1] == sym && gameField[2][2] == sym) {
+            return true;
+        }
+
+
+        // Проверка вертикальных позиций на наличие трех подряд ячеек
+
+        if (gameField[0][0] == sym && gameField[1][0] == sym && gameField[2][0] == sym) {
+            return true;
+        }
+        if (gameField[0][1] == sym && gameField[1][1] == sym && gameField [2][1] == sym) {
+            return true;
+        }
+        if (gameField[0][2] == sym && gameField[1][2] == sym && gameField[2][2] == sym) {
+            return true;
+        }
+
+
+        //Проверка диагоналей на наличие трех ячеек подряд
+
+        if (gameField[0][0] == sym && gameField[1][1] == sym && gameField[2][2] == sym) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        Document doc = factory.newDocumentBuilder().newDocument();
+
+        Element root = doc.createElement("Gameplay");
+        doc.appendChild(root);
+
+
+
+        System.out.println("Введите имя первого игрока:");
+        firstPlayerName = scanner.nextLine();
+        Player one = new Player(1, firstPlayerName, firstPlayerSign);
+        System.out.println("Введите имя второго игрока:");
+        secondPlayerName = scanner.nextLine();
+        Player two = new Player(2, secondPlayerName, secondPlayerSign);
+        //fileWork("Player " + "id=" + one.getId() + "name=" + one.getName() + "symbol= " + one.getSymbol() + "\n");
+        //fileWork("Player " + "id=" + two.getId() + "name=" + two.getName() + "symbol= " + two.getSymbol());
+
+
+        root.appendChild(ParserXml.getPlayer(doc, one.getId() + "", one.getName(), one.getSymbol() + ""));
+        root.appendChild(ParserXml.getPlayer(doc, two.getId() + "", two.getName(), two.getSymbol() + ""));
+
+
+
+        Element game = doc.createElement("Game");
+        root.appendChild(game);
+        while (repeat) {
+            initialPlayField();
+            printField();
+
+            while (true) {
+                firstPlayerStep();
+                one.setLocation(location);
+                System.out.println("Step " + "num= " + Player.step + " playerId= " + one.getId() + " делает ход в локацию " + one.getLocation());
+                game.appendChild(ParserXml.getStep(doc, Player.step + "", one.getId() + "", one.getLocation()));
+                printField();
+                if (checkWin(firstPlayerSign)) {
+                    game.appendChild(ParserXml.getResult(doc,one.getId() + "", one.getName(), one.getSymbol() + "" ));
+                    System.out.println(firstPlayerName + " победил(a)!");
+                    firstCount +=3 ;
+                    break;
+                }
+                if (isFieldFull()) {
+                    System.out.println("Ничья!");
+                    game.appendChild(ParserXml.getDraw(doc));
+                    firstCount++;
+                    break;
+                }
+                Player.step++;
+
+                secondPlayerStep();
+                two.setLocation(location);
+                System.out.println("Step " + "num= " + Player.step + " playerId= " + two.getId() + " делает ход в локацию " + two.getLocation());
+                game.appendChild(ParserXml.getStep(doc, Player.step + "", two.getId() + "", two.getLocation()));
+                printField();
+                if (checkWin(secondPlayerSign)) {
+                    game.appendChild(ParserXml.getResult(doc,two.getId() + "", two.getName(), two.getSymbol() + "" ));
+                    System.out.println(secondPlayerName + " победил(a)!");
+                    secondCount += 3;
+                    break;
+                }
+                if (isFieldFull()) {
+                    System.out.println("Ничья!");
+                    game.appendChild(ParserXml.getDraw(doc));
+                    secondCount++;
+                    break;
+                }
+                Player.step++;
+            }
+            System.out.println("Хотите сыграть еще?");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String answer = reader.readLine();
+            if (answer.equals("No")) {
+                repeat = false;
+            }
+            Player.step = 1;
+        }
+        //fileWork(firstPlayerName, secondPlayerName, firstCount, secondCount);
+        /*System.out.println(one.getName() + " " + one.getSymbol() + " " + one.getId());
+        System.out.println("Игрок 1 совершил " + one.count + " ходов.");
+        System.out.println(two.getName() + " " + two.getSymbol() + " " + two.getId());
+        System.out.println("Игрок 2 совершил " + two.count + " ходов.");*/
+
+        File file = new File("D:\\test.xml");
+
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(new DOMSource(doc), new StreamResult(file));
+
+    }
+}
---
+++ ParserXml.java
@@ -0,0 +1,76 @@
+package theGame;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.dom.DOMSource;
+import java.io.File;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Node;
+
+public class ParserXml {
+    public static void main(String[] args) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        Document doc = factory.newDocumentBuilder().newDocument();
+
+       /* Element root = doc.createElement("Gameplay");
+        doc.appendChild(root);
+
+        Element player = doc.createElement("Player");
+        player.setAttribute("id", "1");
+        player.setAttribute("name", "Playerr");
+        player.setAttribute("symbol", "X");
+        root.appendChild(player);
+
+        Element game = doc.createElement("Game");
+        root.appendChild(game);
+
+        Element step = doc.createElement("Step");
+        step.setAttribute("val", "3");
+        game.appendChild(step); */
+
+        //Element result = doc.createElement("GameResult");
+        /*root.appendChild(result);
+        result.appendChild(getPlayer(doc, "2", "NAME", "O"));*/
+
+
+    }
+
+    public static Node getDraw(Document doc) {
+        Element result = doc.createElement("GameResult");
+        result.setAttribute("Draw", "Draw");
+        return result;
+    }
+
+    public static Node getResult(Document doc, String id, String name, String symbol) {
+        Element result = doc.createElement("GameResult");
+
+        result.appendChild(getPlayer(doc, id, name, symbol));
+
+        return result;
+    }
+
+    public static Node getStep(Document doc, String num, String playerId, String location) {
+        Element step = doc.createElement("Step");
+        step.setAttribute("num", num);
+        step.setAttribute("playerId", playerId);
+        step.setAttribute("location", location);
+        return step;
+        //game.appendChild(step);
+    }
+
+    public static Node getPlayer (Document doc, String id, String name, String symbol) {
+        Element player = doc.createElement("Player");
+
+        player.setAttribute("id", id);
+
+        player.setAttribute("name", name);
+
+        player.setAttribute("symbol", symbol);
+        return player;
+    }
+}
