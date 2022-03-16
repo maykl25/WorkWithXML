d55275b4f7fc81c7fbde177186a92192770fbbc0
Сергей Шураев <70658716+maykl25@users.noreply.github.com>
2022-03-16 21:47:42 +0200

    Add files via upload
    Код, вытаскивающий значение шагов из файла XML.

---
+++ ReadFileWithXml.java
@@ -0,0 +1,39 @@
+package theGame;
+
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import java.util.ArrayList;
+
+public class ReadFileWithXml {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printCost(Document document, ArrayList<String> steps) throws DOMException, XPathExpressionException {
+
+        XPathFactory pathFactory = XPathFactory.newInstance();
+        XPath xpath = pathFactory.newXPath();
+
+        XPathExpression expr = xpath.compile("//Game/Step");
+
+        NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node n = nodes.item(i);
+            steps.add(n.getTextContent());
+
+        }
+        System.out.println();
+    }
+
+}
